99e332e4f7597f0b5e81949fa63d91fb3af9f44a
Romuald Quantin <romu@soundstep.com>
2022-03-11 22:09:19 +0000

    Added calculator.

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # rust-playground
+
 Playground for rust
+
+## Notes
+
+### Run
+
+```
+cargo run -- myArg
+```
+
+### Build
+
+```
+cargo build
+/target/debug/calculator myArg
+```
+
+or
+
+```
+rustc ./src/main.rs
+./main myArg
+```
+
+### Release
+
+```
+cargo build --release
+./target/release/calculator myArg
+```
+
+### Watch and run
+
+```
+cargo install cargo-watch
+cargo watch -x run
+// watch and send arguments
+cargo watch -x 'run -- myArg'
+// watch and clear console:
+cargo watch -c -w src -x run
+```
--- calculator/src/main.rs
+++ calculator/src/main.rs
@@ -1,3 +1,39 @@
+use std::env::{args, Args};
+
 fn main() {
-    println!("Hello, world!");
+    let mut args: Args = args();
+    println!("args {:?}", args);
+    let first = args.nth(1).unwrap();
+    let operator = args.nth(0).unwrap().chars().next().unwrap();
+    let second = args.nth(0).unwrap();
+    println!("Calculating... {} {} {}", first, operator, second);
+    let first_number = first.parse::<f32>().unwrap();
+    let second_number: f32 = second.parse().unwrap();
+    let result = operate(operator, first_number, second_number);
+    println!("Result: {:?}", output(first_number, operator, second_number, result));
+}
+
+fn operate(operator: char, first_number: f32, second_number: f32) -> f32 {
+    // if operator == '+' {
+    //     first_number + second_number
+    // } else if operator == '-' {
+    //     first_number - second_number
+    // } else if operator == '/' {
+    //     first_number / second_number
+    // } else if operator == '*' {
+    //     first_number * second_number
+    // } else {
+    //    0.0
+    // }
+    match operator {
+        '+' => first_number + second_number,
+        '-' => first_number - second_number,
+        '/' => first_number / second_number,
+        '*' | 'x' | 'X' => first_number * second_number,
+        _ => panic!("Invalid operator used.")
+    }
+}
+
+fn output (first_number: f32, operator: char, second_number: f32, result: f32) -> String {
+    format!("{} {} {} = {}", first_number, operator, second_number, result)
 }
