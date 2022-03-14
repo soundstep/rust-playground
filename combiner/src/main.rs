4dd45b59d8d2976d379c2b29a928873f1a9d8b26
Romuald Quantin <romu@soundstep.com>
2022-03-14 12:41:49 +0000

    Added image input

--- combiner/src/main.rs
+++ combiner/src/main.rs
@@ -1,7 +1,65 @@
 mod args;
 use args::Args;
+use image::{ io::{ Reader }, ImageFormat, DynamicImage, imageops::FilterType::Triangle, GenericImageView };
+use std::{ io::BufReader, fs::File };
+use std::convert::TryInto;
 
-fn main() {
+#[derive(Debug)]
+enum ImageDataErrors {
+    DifferentImageFormats,
+}
+
+struct FloatingImage {
+    width: u32,
+    height: u32,
+    data: Vec<u8>,
+    name: String,
+}
+
+impl FloatingImage {
+    fn new(width: u32, height: u32, name: String) -> Self {
+        let buffer_capacity = width * height * 4;
+        let buffer = Vec::with_capacity(buffer_capacity.try_into().unwrap());
+        FloatingImage {
+            width,
+            height,
+            data: buffer,
+            name,
+        }
+    }
+}
+
+fn main() -> Result<(), ImageDataErrors> {
     let args = Args::new();
-    println!("args {:?}", args);
+    let (image_1, image_format_1) = find_image_from_path(args.image_1);
+    let (image_2, image_format_2) = find_image_from_path(args.image_2);
+    if image_format_1 != image_format_2 {
+        return Err(ImageDataErrors::DifferentImageFormats);
+    }
+    let (image_1, image_2) = standardise_size(image_1, image_2);
+    let output = FloatingImage::new(image_1.width(), image_2.height(), args.output);
+    Ok(())
+}
+
+fn find_image_from_path (path: String) -> (DynamicImage, ImageFormat) {
+    let image_reader: Reader<BufReader<File>> = Reader::open(path).unwrap();
+    let image_format: ImageFormat = image_reader.format().unwrap();
+    let image: DynamicImage = image_reader.decode().unwrap();
+    (image, image_format)
+}
+
+fn get_smallest_dimensions (dim_1: (u32, u32), dim_2: (u32, u32)) -> (u32, u32) {
+    let pix_1 = dim_1.0 * dim_1.1;
+    let pix_2 = dim_2.0 * dim_2.1;
+    return if pix_1 < pix_2 { dim_1 } else { dim_2 };
+}
+
+fn standardise_size (image_1: DynamicImage, image_2: DynamicImage) -> (DynamicImage, DynamicImage) {
+    let (width, height) = get_smallest_dimensions(image_1.dimensions(), image_2.dimensions());
+    println!("Width: {}, Height: {}\n", width, height);
+    if image_2.dimensions() == (width, height) {
+        (image_1.resize_exact(width, height, Triangle), image_2)
+    } else {
+        (image_1, image_2.resize_exact(width, height, Triangle))
+    }
 }
