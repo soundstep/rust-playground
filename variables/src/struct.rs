f71427920cbf713a9be68e2d7c94f99367098211
Romuald Quantin <romu@soundstep.com>
2022-03-17 13:27:39 +0000

    Struct in progress.

--- variables/src/struct.rs
+++ variables/src/struct.rs
@@ -8,6 +8,15 @@ struct Rectangle {
     height: u32,
 }
 
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
 fn main() {
     let scale = 2;
     let rect1 = Rectangle {
@@ -15,10 +24,15 @@ fn main() {
         height: 50,
     };
 
-    // println!(
-    //     "The area of the rectangle is {} square pixels.",
-    //     area(&rect1)
-    // );
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        area(&rect1)
+    );
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        rect1.area()
+    );
 
     println!("rect1 is {:#?}", rect1);
 }
