35c7bf695a868ecb68de9d3fbc4706f7e4e03d7f
Romuald Quantin <romu@soundstep.com>
2022-06-01 08:28:33 +0100

    Added string and enum.

--- Learn-Rust-by-Building-Real-Applications/server/src/main.rs
+++ Learn-Rust-by-Building-Real-Applications/server/src/main.rs
@@ -1,5 +1,5 @@
 fn main() {
-    let server = Server::new("127.0.0.1:8080");
+    let server = Server::new("127.0.0.1:8080".to_string());
     server.run();
 }
 
@@ -11,5 +11,31 @@ impl Server {
     fn new(addr: String) -> Self {
         Self { addr }
     }
-    fn run(self) {}
+    fn run(self) {
+        println!("Listening on {}", self.addr);
+    }
 }
+
+struct Request {
+    path: String,
+    query_string: Option<String>,
+    method: Method,
+}
+
+enum Method {
+    GET,
+    DELETE,
+    POST,
+    PUT,
+    HEAD,
+    CONNECT,
+    OPTIONS,
+    TRACE,
+    PATCH,
+}
+
+/*
+GET /user?id=10 HTTP/1.1\r\n
+HEADERS \r\n
+BODY
+*/
